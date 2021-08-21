e75c59e0b5b596ad0d7686790f3d065da13f3c57
TheHebi <nturcotte8@live.com>
2021-08-21 12:12:45 -0700

    react components, gitignore fix

---
+++ src/components/About/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css.css';
+
+export default function About() {
+
+  return (
+    <div></div>
+  );
+}
---
+++ src/components/Contact/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css.css';
+
+export default function Contact() {
+
+  return (
+    <div></div>
+  );
+}
