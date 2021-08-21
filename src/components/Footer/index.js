e6ac771336149e04ccf46ccea646595308440a09
TheHebi <nturcotte8@live.com>
2021-08-21 13:20:14 -0700

    change contact section to footer button

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,6 +7,7 @@ export default function Footer() {
       <footer className="footer bg-dark fixed-bottom">
         <a href="https://github.com/TheHebi"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
         <a href="https://www.linkedin.com/in/nathanielturcotte/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href="mailto: nturcotte8@live.com"><img src="https://img.icons8.com/color/48/000000/email.png" alt="Email" className="icon"/></a>
     </footer>
   );
 }
