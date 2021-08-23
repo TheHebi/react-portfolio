8e12c8ce74d4f2647f4104b97db5458aec5875af
TheHebi <nturcotte8@live.com>
2021-08-23 14:11:58 -0700

    fix links not opening in seperate window for project cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -10,14 +10,14 @@ export default function Portfolio(props) {
       <div className="content">
         <p className="card-title">{props.name}</p>
         <p className="proj-icons-container">
-          <a href={props.github}>
+          <a href={props.github} target="_blank" rel="noreferrer">
             <img
               className="project-icon"
               src="https://img.icons8.com/fluent/48/000000/github.png"
               alt="GitHub Repo"
             />
           </a>
-          <a href={props.deploy}>
+          <a href={props.deploy} target="_blank" rel="noreferrer">
             <img
               className="project-icon"
               src="https://img.icons8.com/fluent/48/000000/domain.png"
