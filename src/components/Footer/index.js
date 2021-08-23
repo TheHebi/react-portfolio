1c4f0c259783d3eeaa2ed47f903d9be9c48545bd
TheHebi <nturcotte8@live.com>
2021-08-23 13:41:21 -0700

    fix contact links to open in new tab

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,9 @@ export default function Footer() {
 
   return (
       <footer className="footer bg-dark fixed-bottom">
-        <a href="https://github.com/TheHebi"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
-        <a href="https://www.linkedin.com/in/nathanielturcotte/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-        <a href="mailto: nturcotte8@live.com"><img src="https://img.icons8.com/color/48/000000/email.png" alt="Email" className="icon"/></a>
+        <a href="https://github.com/TheHebi" target="_blank" rel="noreferrer" ><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/nathanielturcotte/" target="_blank" rel="noreferrer" ><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href="mailto: nturcotte8@live.com" target="_blank" rel="noreferrer" ><img src="https://img.icons8.com/color/48/000000/email.png" alt="Email" className="icon"/></a>
     </footer>
   );
 }
